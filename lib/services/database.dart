9265e1a7e941a02c8e01a5a9806d07aab03770d8
anishdhakal54 <anish.dhakal54@gmail.com>
2020-11-24 16:29:27 +0545

    firestore database completed

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/foundation.dart';
+import 'package:time_tracker/app/home/model/job.dart';
+import 'package:time_tracker/services/api_path.dart';
+import 'package:time_tracker/services/firestore_service.dart';
 
-abstract class Database {}
+abstract class Database {
+  Future<void> createJob(Job job);
+
+  Stream<List<Job>> getJobs();
+}
 
 class FirebaseDatabase implements Database {
   String uid;
+
   FirebaseDatabase({@required this.uid}) : assert(uid != null);
+
+  final _service = FirestoreService.instance;
+
+  Future<void> createJob(Job job) async => await _service.setData(
+        path: ApiPath.job(uid, 'job_abc'),
+        data: job.toMap(),
+      );
+
+  Stream<List<Job>> getJobs() => _service.collectionStream(
+      path: ApiPath.jobs(uid), builder: (data) => Job.fromMap(data));
 }
